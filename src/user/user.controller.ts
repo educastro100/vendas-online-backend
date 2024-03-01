3b11601e811b0393471e470fda0b3391765f2f0b
80913060 <eduardo.castro@zukk.com.br>
2024-02-29 21:43:22 -0300

    feat: User Service and getAllUsers method

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,19 +1,25 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
+import { UserService } from './user.service';
+import { User } from './interfaces/user.interface';
 
 @Controller('user')
 export class UserController {
 
+constructor(private readonly userService: UserService){}
+
+// Função de cadastro
 @Post()
 async createUser( 
-   @Body() createUser: CreateUserDto
-   ){
+   @Body() createUser: CreateUserDto){
       // Declara o objeto com o DTO
-      const newUser = {
-         ...createUser,
-         password: undefined, // Remove o password do retorno.
-      }
-      return newUser
+      return this.userService.createUser(createUser)
+}
+
+// Função de listar
+@Get()
+async getAllUser() : Promise<User[]>{
+   return this.userService.getAllUser();
 }
 
 }
