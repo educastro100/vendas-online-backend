afdc081babd430c5812e5835d248507cac5dfdef
80913060 <eduardo.castro@zukk.com.br>
2024-02-29 22:06:07 -0300

    feat: Environment Configuration and DB connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 
 @Module({
-  imports: [UserModule],
+  imports: 
+  // Config do environment
+  [ConfigModule.forRoot({
+    envFilePath: ['.env.development.local'],
+  }),
+
+  // Config do banco
+  TypeOrmModule.forRoot({
+    type: 'postgres',
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    password: process.env.DB_PASSWORD,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USERNAME
+  })
+
+  // Outros Módulos
+
+  ,UserModule],
   controllers: [],
   providers: [],
 })
